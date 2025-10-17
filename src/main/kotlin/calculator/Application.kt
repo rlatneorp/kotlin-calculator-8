659547d85ab2302191389819f5c8b3d9348e8753
rlatneorp@gmail.com <rlatneorp@gmail.com>
2025-10-17 21:16:33 +0900

    feat: 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한다

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -5,7 +5,8 @@ import camp.nextstep.edu.missionutils.Console
 fun main() {
     // TODO: 프로그램 구현
     println("덧셈할 문자열을 입력해 주세요.")
-    val basicSeparator = arrayOf(",", ";")
     val choiceNum = Console.readLine()
-
+    val noneSeparatorNum = choiceNum?.replace(";", ",")
+    val number = noneSeparatorNum?.split(",")
+    print(number)
 }
