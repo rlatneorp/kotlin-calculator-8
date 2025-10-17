33f114cc66434688eb00e09ccd93aaf05fed7d53
rlatneorp@gmail.com <rlatneorp@gmail.com>
2025-10-18 08:03:13 +0900

    refactor: 불필요한 함수 제거

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,15 +6,24 @@ fun main() {
     // TODO: 프로그램 구현
     println("덧셈할 문자열을 입력해 주세요.")
     val choiceNum = Console.readLine()
-    val noneSeparatorNum = choiceNum?.replace(";", ",")
-    println(noneSeparatorNum)
+    val customStr = choiceNum[2].toString()
+    println(customStr)
+    var noneSeparatorNum: String? = ""
+    if (choiceNum.contains(";")) {
+        noneSeparatorNum = choiceNum?.replace(";", ",")
+    }
+    if (!choiceNum.contains(",")) {
+        noneSeparatorNum = (choiceNum.substring(5 until choiceNum.length)).replace(customStr, ",")
+    }
+
     val number = noneSeparatorNum?.split(",")
-    println(number)
     var result = 0
+
     if (number != null) {
         for (i in number) {
             result += i.toInt()
         }
     }
-    print(result)
+    println("결과 : $result")
 }
+
