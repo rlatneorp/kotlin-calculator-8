6f21b682fecb2ea5c04aa73380f6b64f0970dbd9
rlatneorp@gmail.com <rlatneorp@gmail.com>
2025-10-18 08:03:13 +0900

    feat: 커스텀 구분자 추출 customSeparator 함수 구현

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,10 +6,9 @@ fun main() {
     // TODO: 프로그램 구현
     println("덧셈할 문자열을 입력해 주세요.")
     val choiceNum = Console.readLine()
+    val customStr = customSeparator(choiceNum)
     val noneSeparatorNum = choiceNum?.replace(";", ",")
-    println(noneSeparatorNum)
     val number = noneSeparatorNum?.split(",")
-    println(number)
     var result = 0
     if (number != null) {
         for (i in number) {
@@ -17,4 +16,9 @@ fun main() {
         }
     }
     print(result)
+}
+
+fun customSeparator(choiceNum: String): String {
+    val customStr = choiceNum.substring(2)
+    return customStr
 }
