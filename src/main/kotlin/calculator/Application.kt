4bc2eb174cea0ab9e7b665d2e0c34855c7746b7d
rlatneorp@gmail.com <rlatneorp@gmail.com>
2025-10-17 17:58:33 +0900

    feat: 입력 받아서 num 변수에 저장

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -2,6 +2,8 @@ package calculator
 
 fun main() {
     // TODO: 프로그램 구현
-    print("덧셈할 문자열을 입력해 주세요.")
+    println("덧셈할 문자열을 입력해 주세요.")
+    val basicSeparator = arrayOf(",", ";")
+    val num = readLine()
 
 }
