d81fd09064e700fe7a5231a48da47dbb80ec2673
rlatneorp@gmail.com <rlatneorp@gmail.com>
2025-10-20 05:36:47 +0900

    fix: 입력된 숫자가 number 변수에 들어가 계산이 잘 되도록 수정

--- src/main/kotlin/calculator/Application.kt
+++ src/main/kotlin/calculator/Application.kt
@@ -6,24 +6,26 @@ fun main() {
     // TODO: 프로그램 구현
     println("덧셈할 문자열을 입력해 주세요.")
     val choiceNum = Console.readLine()
-    val customStr = choiceNum[2].toString()
-    println(customStr)
-    var noneSeparatorNum: String? = ""
+    var noneSeparatorNum: String? = choiceNum
+
     if (choiceNum.contains(";")) {
         noneSeparatorNum = choiceNum?.replace(";", ",")
     }
-    if (!choiceNum.contains(",")) {
+    if (choiceNum.contains("//") && choiceNum.contains("""\n""")) {
+        val customStr = choiceNum[2].toString()
         noneSeparatorNum = (choiceNum.substring(5 until choiceNum.length)).replace(customStr, ",")
     }
-
+    println(noneSeparatorNum)
     val number = noneSeparatorNum?.split(",")
+    println(number)
     var result = 0
 
-    if (number != null) {
+    if (number != null && !number.contains(",")) {
         for (i in number) {
             result += i.toInt()
         }
     }
+
     println("결과 : $result")
 }
 
